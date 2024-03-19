fc32ab8c6eb817044cfae0baa9efcdeb45460f5c
Denis Kulikov <cduke-ru@yandex.ru>
2024-03-19 20:32:42 +0300

    Add SkipLocalsInitAttribute declaration for NETSTANDARD

--- common/SkipLocalsInit.cs
+++ common/SkipLocalsInit.cs
@@ -1,1 +1,36 @@
 [module: System.Runtime.CompilerServices.SkipLocalsInit]
+
+#if NETSTANDARD2_0 || NETSTANDARD2_1
+namespace System.Runtime.CompilerServices;
+
+/// <summary>
+/// Used to indicate to the compiler that the <c>.locals init</c>
+/// flag should not be set in method headers.
+/// </summary>
+/// <remarks>
+/// This attribute is unsafe because it may reveal uninitialized memory to
+/// the application in certain instances (e.g., reading from uninitialized
+/// stackalloc'd memory). If applied to a method directly, the attribute
+/// applies to that method and all nested functions (lambdas, local
+/// functions) below it. If applied to a type or module, it applies to all
+/// methods nested inside. This attribute is intentionally not permitted on
+/// assemblies. Use at the module level instead to apply to multiple type
+/// declarations.
+/// </remarks>
+[AttributeUsage(AttributeTargets.Module
+    | AttributeTargets.Class
+    | AttributeTargets.Struct
+    | AttributeTargets.Interface
+    | AttributeTargets.Constructor
+    | AttributeTargets.Method
+    | AttributeTargets.Property
+    | AttributeTargets.Event, Inherited = false)]
+[SuppressMessage("Design", "MA0104")]
+[SuppressMessage("Design", "MA0048")]
+internal sealed class SkipLocalsInitAttribute : Attribute
+{
+    public SkipLocalsInitAttribute()
+    {
+    }
+}
+#endif
