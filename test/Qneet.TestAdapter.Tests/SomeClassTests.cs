0b680ff682bd1f99df2055a486d1cfe5c8b77029
Denis Kulikov <cduke-ru@yandex.ru>
2024-11-04 23:16:56 +0300

    update packages

--- test/Qneet.TestAdapter.Tests/SomeClassTests.cs
+++ test/Qneet.TestAdapter.Tests/SomeClassTests.cs
@@ -2,6 +2,7 @@ namespace Qneet.TestAdapter.Tests;
 
 [SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "It`s tests")]
 [SuppressMessage("Minor Code Smell", "S3400:Methods should not return constants", Justification = "It`s tests")]
+[SuppressMessage("Minor Code Smell", "S2325:Methods and properties that don't access instance data should be static", Justification = "<Pending>")]
 public class SomeClassTests
 {
     public int M() => 1;
